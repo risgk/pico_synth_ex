f1e3d0d8ba28fe4af91a17a0863f1da79b8755e1
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2021-08-15 11:51:02 +0900

    ピッチ制御値、ゲートOn制御値をオシレータ、EGから独立

--- pico_synth_ex.c
+++ pico_synth_ex.c
@@ -14,10 +14,10 @@ typedef int16_t Q14; // 小数部14ビットの符号付き固定小数点数
 #define FCLKSYS (120000000) // システムクロック周波数（Hz）
 #define FS      (48000)     // サンプリング周波数（Hz）
 #define FA      (440.0F)    // 基準周波数（Hz）
+
 //////// オシレータ //////////////////////////////
 static uint32_t          Osc_freq_table[121];      // 周波数テーブル
 static Q14               Osc_wave_tables[31][512]; // 波形テーブル群
-static volatile uint32_t Osc_pitch[4];             // ピッチ設定値
 
 static void Osc_init() {
   for (uint32_t pitch = 0; pitch < 121; ++pitch) {
@@ -41,11 +41,13 @@ static void Osc_init() {
     }
   }
 }
-static inline Q28 Osc_process(uint32_t voice) {
-  static uint32_t phase[4]; // 位相
-  phase[voice] += Osc_freq_table[Osc_pitch[voice]];
 
-  Q14* wave_table = Osc_wave_tables[(Osc_pitch[voice] + 3) >> 2];
+static inline Q28 Osc_process(uint32_t voice, uint32_t pitch) {
+  static uint32_t phase[4]; // 位相
+  phase[voice] += Osc_freq_table[pitch];
+
+  uint32_t pitch4 = (pitch + 3) >> 2;
+  Q14* wave_table = Osc_wave_tables[pitch4];
   uint32_t curr_index = phase[voice] >> 23;
   uint32_t next_index = (curr_index + 1) & 0x000001FF;
   Q14 curr_sample = wave_table[curr_index];
@@ -54,9 +56,11 @@ static inline Q28 Osc_process(uint32_t voice) {
   return (curr_sample << 14) +
          ((next_sample - curr_sample) * next_weight);
 }
+
 //////// フィルタ ////////////////////////////////
 struct F_COEFS { Q28 b0_a0, a1_a0, a2_a0; };
 struct F_COEFS Fil_table[8][121]; // フィルタ係数群テーブル
+
 static volatile uint32_t Fil_cut = 120; // カットオフ設定値
 static volatile uint32_t Fil_res = 0;   // レゾナンス設定値
 
@@ -77,6 +81,7 @@ static void Fil_init() {
     }
   }
 }
+
 static inline int32_t mul_32_32_h32(int32_t x, int32_t y) {
   // 32ビット同士の乗算結果の上位32ビット
   int32_t x1 = x >> 16; uint32_t x0 = x & 0xFFFF;
@@ -85,6 +90,7 @@ static inline int32_t mul_32_32_h32(int32_t x, int32_t y) {
   int32_t z = ((x0 * y0) >> 16) + (x1 * y0) + (x0_y1 & 0xFFFF);
   return (z >> 16) + (x0_y1 >> 16) + (x1 * y1);
 }
+
 static inline Q28 Fil_process(uint32_t voice, Q28 x0) {
   static uint32_t f_counter[4];       // フィルタ処理回数
   static uint32_t curr_cut[4];        // カットオフ現在値
@@ -104,27 +110,30 @@ static inline Q28 Fil_process(uint32_t voice, Q28 x0) {
   x1[voice] = x0;        y1[voice] = y0;
   return y0;
 }
+
 //////// アンプ //////////////////////////////////
 static inline int32_t mul_32_16_h32(int32_t x, int16_t y) {
   // 32ビットと16ビットの乗算結果の上位32ビット
   int32_t x1 = x >> 16; uint32_t x0 = x & 0xFFFF;
   return ((x0 * y) >> 16) + (x1 * y);
 }
+
 static inline Q28 Amp_process(uint32_t voice, Q28 in, Q14 gain_in) {
   return mul_32_16_h32(in, gain_in) << 2;
 }
+
 //////// LFO（Low Frequency Oscillator） /////////
 // TODO
-//////// EG（Envelope Generator） ////////////////
-static volatile int32_t EG_gate_on[4]; // EGゲート設定値
 
-static inline Q14 EG_process(uint32_t voice) {
-  static Q14 curr_level[4];                        // レベル現在値
-  Q14        targ_level = EG_gate_on[voice] << 14; // レベル目標値
+//////// EG（Envelope Generator） ////////////////
+static inline Q14 EG_process(uint32_t voice, int32_t gate_on) {
+  static Q14 curr_level[4];              // レベル現在値
+  Q14        targ_level = gate_on << 14; // レベル目標値
   curr_level[voice] =
       targ_level - (((targ_level - curr_level[voice]) * 255) / 256);
   return curr_level[voice];
 }
+
 //////// PWMオーディオ出力部 /////////////////////
 #define PWMA_GPIO  (28)           // PWM出力するGPIO番号
 #define PWMA_SLICE (6)            // PWMスライス番号
@@ -132,6 +141,7 @@ static inline Q14 EG_process(uint32_t voice) {
 #define PWMA_CYCLE (FCLKSYS / FS) // PWM周期
 
 static void pwm_irq_handler();
+
 static void PWMA_init() {
   gpio_set_function(PWMA_GPIO, GPIO_FUNC_PWM);
   irq_set_exclusive_handler(PWM_IRQ_WRAP, pwm_irq_handler);
@@ -141,16 +151,21 @@ static void PWMA_init() {
   pwm_set_chan_level(PWMA_SLICE, PWMA_CHAN, PWMA_CYCLE / 2);
   pwm_set_enabled(PWMA_SLICE, true);
 }
+
 static inline void PWMA_process(Q28 in) {
   int32_t level_int32 = (in >> 18) + (PWMA_CYCLE / 2);
   uint16_t level = (level_int32 > 0) * level_int32;
   pwm_set_chan_level(PWMA_SLICE, PWMA_CHAN, level);
 }
+
 //////// 割り込みハンドラとメイン関数 ////////////
 static volatile uint16_t s_time     = 0; // 開始時間
 static volatile uint16_t max_s_time = 0; // 最大開始時間
 static volatile uint16_t p_time     = 0; // 処理時間
 static volatile uint16_t max_p_time = 0; // 最大処理時間
+
+static volatile uint32_t voice_pitch[4];   // ピッチ制御値（ボイス毎）
+static volatile int32_t  voice_gate_on[4]; // ゲートOn制御値（ボイス毎）
 
 static void pwm_irq_handler() {
   pwm_clear_irq(PWMA_SLICE);
@@ -158,8 +173,8 @@ static void pwm_irq_handler() {
 
   Q28 voice_level[4];
   for (uint32_t voice = 0; voice < 4; ++voice) {
-    Q14 eg_out  = EG_process(voice);
-    Q28 osc_out = Osc_process(voice);
+    Q14 eg_out  = EG_process(voice, voice_gate_on[voice]);
+    Q28 osc_out = Osc_process(voice, voice_pitch[voice]);
     Q28 fil_out = Fil_process(voice, osc_out);
     Q28 amp_out = Amp_process(voice, fil_out, eg_out);
     voice_level[voice] = amp_out;
@@ -172,27 +187,28 @@ static void pwm_irq_handler() {
   max_s_time += (s_time > max_s_time) * (s_time - max_s_time);
   max_p_time += (p_time > max_p_time) * (p_time - max_p_time);
 }
+
 int main() {
   set_sys_clock_khz(FCLKSYS / 1000, true);
   stdio_init_all();
   Osc_init(); Fil_init(); PWMA_init();
 #if 1
-  Osc_pitch[0] = 60; EG_gate_on[0] = 1; 
-  Osc_pitch[1] = 64; EG_gate_on[1] = 1; 
-  Osc_pitch[2] = 67; EG_gate_on[2] = 1; 
-  Osc_pitch[3] = 71; EG_gate_on[3] = 1; 
+  voice_pitch[0] = 60; voice_gate_on[0] = 1; 
+  voice_pitch[1] = 64; voice_gate_on[1] = 1; 
+  voice_pitch[2] = 67; voice_gate_on[2] = 1; 
+  voice_pitch[3] = 71; voice_gate_on[3] = 1; 
 #endif
   while (true) {
     switch (getchar_timeout_us(0)) {
 #if 0
-    case '1': Osc_pitch = 0; Amp_on = 1; break; // ド
-    case '2': Osc_pitch = 1; Amp_on = 1; break; // レ
-    case '3': Osc_pitch = 2; Amp_on = 1; break; // ミ
-    case '4': Osc_pitch = 3; Amp_on = 1; break; // ファ
-    case '5': Osc_pitch = 4; Amp_on = 1; break; // ソ
-    case '6': Osc_pitch = 5; Amp_on = 1; break; // ラ
-    case '7': Osc_pitch = 6; Amp_on = 1; break; // シ
-    case '8': Osc_pitch = 7; Amp_on = 1; break; // ド
+    case '1': voice_pitch = 0; Amp_on = 1; break; // ド
+    case '2': voice_pitch = 1; Amp_on = 1; break; // レ
+    case '3': voice_pitch = 2; Amp_on = 1; break; // ミ
+    case '4': voice_pitch = 3; Amp_on = 1; break; // ファ
+    case '5': voice_pitch = 4; Amp_on = 1; break; // ソ
+    case '6': voice_pitch = 5; Amp_on = 1; break; // ラ
+    case '7': voice_pitch = 6; Amp_on = 1; break; // シ
+    case '8': voice_pitch = 7; Amp_on = 1; break; // ド
     case '0':                Amp_on = 0; break;
 #endif
     case 'z': if (Fil_cut > 0)   { --Fil_cut; } break;
@@ -203,8 +219,8 @@ int main() {
     static uint32_t loop_counter = 0; // ループ回数
     if ((++loop_counter & 0xFFFFF) == 0) {
       printf("p:[%3lu,%3lu,%3lu,%3lu], g:[%ld,%ld,%ld,%ld], c:%3lu, r:%lu, ",
-        Osc_pitch[0], Osc_pitch[1], Osc_pitch[2], Osc_pitch[3],
-        EG_gate_on[0], EG_gate_on[1], EG_gate_on[2], EG_gate_on[3],
+        voice_pitch[0], voice_pitch[1], voice_pitch[2], voice_pitch[3],
+        voice_gate_on[0], voice_gate_on[1], voice_gate_on[2], voice_gate_on[3],
         Fil_cut, Fil_res);
       printf("start:%4u/%4u, processing:%4u/%4u\n",
         s_time, max_s_time, p_time, max_p_time);
